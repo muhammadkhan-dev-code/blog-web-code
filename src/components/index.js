c3e7c0cc28998511c570c0afdd89001ad7ee3fc1
muhammadkhan-dev-code <muhammadkhansw00@gmail.com>
2025-09-22 19:57:24 +0500

    new updated code

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,14 @@
 import Header from './header/Header.jsx'
 import Footer from './footer/Footer.jsx'
+import Container from '../container/Container.jsx'
+import Logo from './Logo.jsx'
+import LogoutBtn from './header/LogoutBtn.jsx'
 
-export  {Header,Footer}
+export  {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+    
+}

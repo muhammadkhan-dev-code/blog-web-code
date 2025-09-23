8081c3340866bc1741b18db2c545511e35d396a8
muhammadkhan-dev-code <muhammadkhansw00@gmail.com>
2025-09-23 17:08:10 +0500

    adding form and slug value

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,16 @@ import Footer from './footer/Footer.jsx'
 import Container from '../container/Container.jsx'
 import Logo from './Logo.jsx'
 import LogoutBtn from './header/LogoutBtn.jsx'
+import RTE from './RTE.jsx'
+import Button from './Button.jsx' 
 
 export  {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Button
     
 }

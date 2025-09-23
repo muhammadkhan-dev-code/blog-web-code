ae7882a5966b985b3a6c8953a0fa1c70c7545fe2
muhammadkhan-dev-code <muhammadkhansw00@gmail.com>
2025-09-23 11:55:55 +0500

    React hook form in production'

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -36,17 +36,50 @@ const Login = () => {
             <Logo width="100%" />
           </span>
         </div>
-        <h2 className="text-center text-2xl font-bold leading-tight
-        ">Sign in to your Account</h2>
+        <h2
+          className="text-center text-2xl font-bold leading-tight
+        "
+        >
+          Sign in to your Account
+        </h2>
         <p className="mt-2 text-center text-base text-black/60">
-        Don&apps;t have any account?&nbsp;
-        <Link to ="/signup"
-        className="font-medium text-primary transition-all duration-200 hover:underline">Signup</Link>
+          Don&apps;t have any account?&nbsp;
+          <Link
+            to="/signup"
+            className="font-medium text-primary transition-all duration-200 hover:underline"
+          >
+            Signup
+          </Link>
         </p>
-        {
-            error&&<p className="text-red-500 text-center mt-8  ">{error}</p>
-        }
-        <form onSubmit={handleSubmit(login)}></form>
+        {error && <p className="text-red-500 text-center mt-8  ">{error}</p>}
+        <form onSubmit={handleSubmit(login)} className="mt-8">
+          <div className="space-y-5">
+            <Input
+              label="Email: "
+              placeholder="Enter your email"
+              type="email"
+              {...register("email", {
+                required: true,
+                validate: {
+                  matchPatern: (value) =>
+                    /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                    "Email address must be a valid address",
+                },
+              })}
+            />
+            <Input
+              label="Password"
+              type="password"
+              placeholder="Enter your password"
+              {...register("password", {
+                required: true,
+              })}
+            />
+            <Button type="submit" className="w-full">
+              Sign in
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   );

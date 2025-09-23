8a82f987847f8b03f4185131bb044fbdb78f1704
muhammadkhan-dev-code <muhammadkhansw00@gmail.com>
2025-09-23 10:51:06 +0500

    Production grade react components

---
+++ src/components/Select.jsx
@@ -0,0 +1,27 @@
+import React, { useId } from "react";
+
+const Select = ({ options=
+    , label, className='', ...props }, ref) => {
+  const id = useId();
+
+  return(
+    <div className="w-full">
+        {label && <label htmlFor={id} className=""></label>}
+         <select
+            {...props} id= {id} ref={ref} 
+            className={`px-3 py-2 rounded-lg bg-white text-black outline-neutral-50
+            focus:bg-gray-50 duration-200 border border-gray-200 w-dull ${className}`}>
+{
+    options?.map((option)=>(
+        <option key={option} value={option}>
+           {option}
+        </option>
+    ))
+}
+            </select>
+
+    </div>
+  )
+};
+
+export default React.forwardRef(Select);
